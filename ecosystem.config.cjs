c50598a47251609d8fc8c265976f48f15b4c067d
Kadyr Gullyyev <kadyr.gullyyew@gmail.com>
2026-02-06 20:43:32 +0100

    C

---
+++ ecosystem.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "egmhoreca-web",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      env: {
+        NODE_ENV: "production",
+        API_BASE_URL: "https://api.egmhoreca.ro/website",
+      },
+    },
+  ],
+};
