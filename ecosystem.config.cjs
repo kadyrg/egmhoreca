b0d829730303fa504f67d1bfe901bbe43585dc2c
Kadyr Gullyyev <kadyr.gullyyew@gmail.com>
2026-02-06 20:59:24 +0100

    C

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   apps: [
     {
       name: "egmhoreca-web",
+      cwd: "/var/www/egmhoreca-web",
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3000",
       env: {
